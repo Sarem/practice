dc52c2397b13085da202b0df03438e065a445a9c
gent.sami@gmail.com <github.13>
2020-01-13 09:25:35 +0330

    controller advice

---
+++ src/main/java/com/snap/practice/contact/ContactServiceAdvice.java
@@ -0,0 +1,21 @@
+package com.snap.practice.contact;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ControllerAdvice
+public class ContactServiceAdvice {
+
+    @ExceptionHandler({ContactNotFoundException.class})
+    public ResponseEntity<String> handleRunTimeException(ContactNotFoundException e) {
+        return error(NOT_FOUND, e);
+    }
+
+    private ResponseEntity<String> error(HttpStatus status, Exception e) {
+        return ResponseEntity.status(status).body(e.getMessage());
+    }
+}
